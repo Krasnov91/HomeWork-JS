97e00080f7ef2c8b17ddf40de5e2cbaaa3ab4979
Iurii Krasnoshtanov <krasnoshtanov10.11.91@gamil.com>
2026-05-25 20:55:31 +0200

    hw-js-2.3

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,28 +7,28 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1; // your code
-let dice2; // your code
-let isWinningDouble; // your code
+// let dice1; // your code
+// let dice2; // your code
+// let isWinningDouble; // your code
 
-dice1 = Math.floor(Math.random() * 6 + 1);
-dice2 = Math.floor(Math.random() * 6 + 1);
-console.log(dice1, dice2);
+// dice1 = Math.floor(Math.random() * 6 + 1);
+// dice2 = Math.floor(Math.random() * 6 + 1);
+// console.log(dice1, dice2);
 
-if (dice1 === dice2 && dice1 > 3 && dice2 > 3) {
-  isWinningDouble = true;
-  console.log(isWinningDouble);
-} else {
-  isWinningDouble = false;
-  console.log(isWinningDouble);
-}
+// if (dice1 === dice2 && dice1 > 3 && dice2 > 3) {
+//   isWinningDouble = true;
+//   console.log(isWinningDouble);
+// } else {
+//   isWinningDouble = false;
+//   console.log(isWinningDouble);
+// }
 
 
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
-} else {
-  console.log('Не выигрышный дубль.')
-}
+// console.log('Первый бросок: ' + dice1)
+// console.log('Второй бросок: ' + dice2)
+// if (isWinningDouble) {
+//   console.log('Выигрышный дубль!')
+// } else {
+//   console.log('Не выигрышный дубль.')
+// }
 
--- lesson-02/03.js
+++ lesson-02/03.js
@@ -7,3 +7,19 @@
 const score = 70 // тестовое значение, можно изменять
 let grade
 // your code
+if (score > 0 && score <= 49) {
+  grade = "f";
+  console.log(grade);
+} else if (score >= 50 && score <= 69) {
+  grade = "d";
+  console.log(grade);
+} else if (score >= 70 && score <= 79) {
+  grade = "c";
+  console.log(grade);
+} else if (score >= 80 && score <= 89) {
+  grade = "b";
+  console.log(grade);
+} else if (score >= 90 && score <= 100) {
+  grade = "a";
+  console.log(grade);
+}
