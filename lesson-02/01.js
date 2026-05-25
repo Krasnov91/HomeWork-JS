5edd9cda90bd10e68baa279a0123fedb6b56f4b9
Iurii Krasnoshtanov <krasnoshtanov10.11.91@gamil.com>
2026-05-25 20:28:05 +0200

    hw-js-2.1

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -8,12 +8,12 @@
 */
 
 // тестовые данные (значения можно менять)
-const isAdmin = false
-const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+const isAdmin = false;
+const isVerifiedUser = true;
+const hasSpecialPermission = true;
+const hasTemporaryPass = false;
 
-let isAccess
+let isAccess;
 // your code
 
 if (
@@ -21,4 +21,6 @@ if (
   (hasSpecialPermission === true || hasTemporaryPass === true)
 ) {
   isAccess = true;
-} 
+} else {
+  isAccess = false;
+}
