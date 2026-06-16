153a077c27cb1d2b87f12786f8fed9041894fec6
Iurii Krasnoshtanov <krasnoshtanov10.11.91@gamil.com>
2026-06-16 12:51:18 +0200

    hw-4-1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,8 +1,8 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage(num1, num2) {
-  return (num1 + num2) / 2;
-}
+// function calculateAverage(num1, num2) {
+//   return (num1 + num2) / 2;
+// }
 
 
 
