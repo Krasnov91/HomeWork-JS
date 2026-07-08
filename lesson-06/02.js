8b4c92f7061bd5f563d8236ca408e7203ba237ef
Iurii Krasnoshtanov <krasnoshtanov10.11.91@gamil.com>
2026-07-08 18:11:33 +0200

    hw-6-2

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,7 +24,15 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-
+function map(array, callback) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i)) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
 
 const numbers = [1, 2, 3, 4, 5]
 
